7c27c416a28977b2a7a2fe3042979063c3c9885d
wille44 <willymail@me.com>
2016-10-07 14:17:43 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
+var today = new date();
+var hournow = today.gethour();
+var greetings; 
 
+if (hournow > 18) {
+  greeting = ' good evening';
+} else if (hourNow > 12) {
+  greeting = ' good afternoon';
+} else if (hourNow > 0) {
+  greeting = ' good morning';
+} else {
+  greeting = 'welcome';
+}
+
+document.wirte('<h3>' + greeting + '</h3>');
